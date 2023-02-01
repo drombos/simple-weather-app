8705c676eb0d435da99d398b061cc29b707d9705
krzy1976 <krzysztof.wojciech.dziedzic@gmail.com>
2023-02-01 16:07:01 +0100

    Trochę dopracowałem menu użytkownika.

--- WeatherApp/src/main/java/org/example/Menu.java
+++ WeatherApp/src/main/java/org/example/Menu.java
@@ -7,39 +7,40 @@ public class Menu {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String userOptions="a";
+        String userOptions = "a";
 
         while (!userOptions.equals("d")) {
-        System.out.println("""
-                Wybierz opcję:
-                a. Dodaj lokalizację.
-                b. Wyświetl wszystkie lokalizacje.
-                c. Pobierz prognozę pogody.
-                d. Zakończ program.       
-                """);
-        userOptions = scanner.nextLine();
+            System.out.println("""
+                    Wybierz opcję:
+                    a. Dodaj lokalizację.
+                    b. Wyświetl wszystkie lokalizacje.
+                    c. Pobierz prognozę pogody.
+                    d. Zakończ program.       
+                    """);
+            userOptions = scanner.nextLine();
 
-        switch (userOptions) {
-            case "a":
-                System.out.println("Wybrano opcję 1");
-                break;
-            case "b":
-                System.out.println("Wybrano opcję 2");
-                break;
-            case "c":
-                System.out.println("Wybrano opcję 3");
-                break;
-            case "d":
-                System.out.println("Koniec programu");
-                break;
-            default:
-                System.out.println("Wprowadź litery od a-d: ");
-        }
+            switch (userOptions) {
+                case "a":
+                    System.out.println("Wybrano opcję 1.");
+                    System.out.println();
+                    break;
+                case "b":
+                    System.out.println("Wybrano opcję 2.");
+                    System.out.println();
+                    break;
+                case "c":
+                    System.out.println("Wybrano opcję 3.");
+                    System.out.println();
+                    break;
+                case "d":
+                    System.out.println("Koniec programu.");
+                    break;
+                default:
+                    System.out.println("Wprowadź litery a, b, c lub d: ");
+                    System.out.println();
+            }
         }
     }
-
-
-
 
 
 }
